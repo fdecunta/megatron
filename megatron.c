6ed9caad30534df42a07b31f319582b1485cf726
Facundo Decunta <fdecunta@agro.uba.ar>
2026-08-03 22:45:55 -0300

    go

--- megatron.c
+++ megatron.c
@@ -1,18 +1,3 @@
-/* NOTES:
-	cmd:
-	q        quit
-	p        print node
-	b        go back
-	h        print history
-	l        jump to last watched
-	[0-9]o   open directory
-	[0-9]r   reproduce video with that number
-
-	Example: 
-	reproduce video with index 4
-	4r
-*/
-
 #include <ctype.h>
 #include <dirent.h>
 #include <libgen.h>
@@ -25,7 +10,6 @@
 
 #define MAXNAME_LEN 255      /* path name must be no longer than this */
 #define MAX_CHILDS  256      /* nodes max children nodes */
-#define MAX_INPUT   4        /* max input buffer */
 
 struct node {
 	int type;
@@ -35,37 +19,22 @@ struct node {
 	int n_children;
 };
 
-struct command {
-	long addr;
-	char cmd;
-	int err;
-};
-
 enum { OK = 0, ERR_BAD_CMD = -1 };
 
-const char *cmd_list = "qpbhlor";
+void 	usage(void);
+void 	walk_dir(struct node *n); 
+void 	join_path(char *dst, char *basename, char *filename, int d_type);
+struct node * 	new_node(int d_type, char *filename, struct node *parent);
+void 	free_tree(struct node *n);
 
 
-
-void usage(void);
-void walk_dir(struct node *n); 
-void join_path(char *dst, char *basename, char *filename, int d_type);
-struct node *new_node(int d_type, char *filename, struct node *parent);
-void free_tree(struct node *n);
-
-void parse_command(char *input, struct command *cmd);
-void set_err(struct command *cmd, int err);
-
-void print_screen(struct node *n);
-void print_help(void);
-void print_err(struct command *cmd);
+void print_node(struct node *n);
 
 
 int main(int argc, char *argv[]) 
 {
 	int ch;
 	char *dir = NULL;
-	char input[MAX_INPUT];
 
 	while ((ch = getopt(argc, argv, "d:")) != -1) {
 		switch (ch) {
@@ -103,37 +72,9 @@ int main(int argc, char *argv[])
 	walk_dir(root);
 
 	struct node *cur_node = root;
-	print_screen(cur_node);
 
-	memset(input, '\0', MAX_INPUT);
-	struct command cmd = {-1, -1, ERR_BAD_CMD};
-
-	int running = 1;
-	while (running == 1) {
-		printf("megatron> ");
-
-		memset(input, '\0', MAX_INPUT);
-		fgets(input, MAX_INPUT, stdin);
-		input[MAX_INPUT - 1] = '\0';
-
-		parse_command(input, &cmd);
-
-		if (cmd.err != 0) {
-			print_err(&cmd);
-			continue;
-		}
-
-		switch(cmd.cmd) {
-		case 'p':
-			print_screen(cur_node);
-			break;
-		case 'q':
-			running = 0;
-			break;
-		default:
-			break;
-		}
-	}
+	print_node(root);
+	
 
 	free_tree(root);
 	return 0;
@@ -214,17 +155,6 @@ void free_tree(struct node *n)
 		free(n);
 }
 
-void print_screen(struct node *n)
-{
-	printf(" === %s === \n", n->path);
-	for (int i=0; i < n->n_children; i++) {
-		char *p = strdup(n->children[i]->path);
-		printf("%4d  %s\n", i, basename(p));
-		free(p);
-	}
-	printf("\n");
-}
-
 void print_help(void)
 {
 	// TODO! //
@@ -232,39 +162,14 @@ void print_help(void)
 	puts("\t your buddy");
 }
 
-
-void parse_command(char *input, struct command *cmd)
+void print_node(struct node *n)
 {
-	/* strip newline, truncate at space */
-	char *nl;
-	if ((nl = strchr(input, '\n')) != NULL) { *nl = '\0'; } 
-
-	char *p_cmd = NULL;
-	long addr = strtol(input, &p_cmd, 10);
-
-	if ((strchr(cmd_list, p_cmd[0])) == NULL) {
-		set_err(cmd, ERR_BAD_CMD);
-		return;
-	} 
-		
-	cmd->addr = addr;
-	cmd->cmd = p_cmd[0];
-	cmd->err = OK;
-}
-
-
-void set_err(struct command *cmd, int err)
-{
-	if (err == ERR_BAD_CMD) {
-		cmd->addr = -1;
-		cmd->cmd = 'e';
-		cmd->err = ERR_BAD_CMD;
-	}
-}
-
-void print_err(struct command *cmd)
-{
-	if (cmd->err == ERR_BAD_CMD) {
-		puts("bad command");
+	printf("==> %s <==\n", n->path);
+	for (int i=0; i < n->n_children; i++) {
+		if (n->children[i]->type == DT_DIR) {
+			print_node(n->children[i]);
+		} else {
+			puts(n->children[i]->path);
+		}
 	}
 }
